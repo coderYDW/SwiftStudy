b120d9589c0bdbb99233d6176fcfb1ce823e36fd
DovYoung <1591759055@qq.com>
2020-05-16 14:42:19 +0800

    元组

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -14,15 +14,64 @@ import PlaygroundSupport
 //let nav = UINavigationController(rootViewController: vc)
 //PlaygroundPage.current.liveView = nav
 
-var aaa: String = "name"
+//常量和变量
 
-let bbb : String = "name"
+//var aaa: String = "name"
+//
+//let bbb : String = "name"
+//
+//let i: UInt8 = 255
+//
+//aaa = "age"
+//
+//print("\(bbb) and \(i)")
 
-aaa = "age"
+//基本数据类型
 
-print("\(bbb)")
+//let i = 1
+//
+//if i == 1 {
+//    print(i)
+//}
+//
+//let a: Bool = true
+//
+//if a {
+//    print(a)
+//}
 
+//typealias YDW = Int
+//let a: YDW = 90
 
+//元组
+//let error = (Int: 1, String: "错误信息")
+//print(error)
+//print(error.0)
+//print(error.1)
 
+//let error = (errorCode: 1, errorMessage: "错误信息")
+//print(error)
+//print(error.0)
+//print(error.1)
+//print(error.errorCode, error.errorMessage)
 
+//元组的分解
+//let (errorCode, errorMessage) = error
+//print(errorCode, errorMessage)
 
+//var error = (errorCode: 1, errorMessage: "错误信息")
+//error.errorCode = 2
+//error.errorMessage = "没有权限"
+//print(error)
+
+//var error: (errorCode: Int, errorMessage: String) = (1, "name")
+//var error: (errorCode: Int, errorMessage: String) = (errorCode: 1, errorMessage:"name")
+//
+//print(error)
+
+func func001 (name: String) -> (errorCode: Int, errorMessage: Any) {
+    return (1, "错误信息")
+}
+var error = func001(name: "Hello")
+error.errorMessage = 3
+print(error)
