740359098885c113f5682f467c667eae81e6ba3f
DovYoung <1591759055@qq.com>
2020-05-18 13:02:03 +0800

    运算符重载和自定义运算符
    运算符重载
    自定义运算符并指定优先级
    自定义优先级

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -195,18 +195,241 @@ import PlaygroundSupport
 //print(b)
 
 //合并空值运算符
-var a: Int? = 9
-print(a!)
-//当a等于nil的时候强制展开会报错
-a = nil
+//var a: Int? = 9
 //print(a!)
-print(a ?? 10)
+////当a等于nil的时候强制展开会报错
+//a = nil
+////print(a!)
+//print(a ?? 10)
+//
+//func getNumber () -> Int {
+//    print("GetNumber")
+//    return 8
+//}
+//var b: Int? = 10
+//print(b ?? getNumber()) //方法没有被执行
+//b = nil
+//print(b ?? getNumber()) //方法被执行
 
-func getNumber () -> Int {
-    print("GetNumber")
-    return 8
+//区间运算符
+//顺序
+//for i in 0...5 {
+//    print(i)
+//}
+//倒序
+//for i in (0...5).reversed()  {
+//    print(i)
+//}
+
+//let names = ["yagn","wagn", "li", "liu"]
+//for name in names[2...] {
+//    print(name)
+//}
+
+//单侧区间
+//let range = ...5
+//print(range.contains(7))
+//print(range.contains(5))
+//print(range.contains(-1))
+//print(range.contains(7))
+
+//索引区间
+//var welcome = "hello,world"
+//let range  = welcome.index(welcome.endIndex, offsetBy: -6) ..< welcome.endIndex
+//welcome.removeSubrange(range)
+//print(welcome)
+////字符串区间
+//let range1 = "a"..."z"
+//for char in welcome {
+//    if !range1.contains(String(char)) {
+//        print("\(char)不是小写字母")
+//    }
+//}
+
+
+//两个数字进行交换
+
+//func exchangeTwoNum (a: Int, b: Int) -> (Int, Int) {
+//    var a1 = a
+//    var b1 = b
+//    a1 = a1 ^ b1
+//    b1 = a1 ^ b1
+//    a1 = a1 ^ b1
+//    return (a1, b1)
+//}
+//print(exchangeTwoNum(a: 3, b: 7))
+
+
+//求int数二进制中1的个数
+//遍历次数较多,有多少位数就遍历多少次
+//func getCountOnes01 (num: UInt) -> UInt {
+//    var count: UInt = 0
+//    var temp = num
+//    while temp != 0 {
+////        let a = temp & 1
+////        if a == 1 {
+////            count += 1
+////        }
+//        count += temp & 1
+//        temp = temp >> 1
+//    }
+//    return count
+//}
+//print(getCountOnes01(num: 6))
+
+//改进方法遍历次数少,有多少个1就遍历多少次
+//将一个数与自身减一按位与运算,会消除最低位的1
+//func getCountOnes02(num: UInt) -> UInt {
+//    var count: UInt = 0
+//    var temp = num
+//    while temp != 0 {
+//        count += 1
+//        temp = temp & (temp - 1)
+//    }
+//    return count
+//}
+//print(getCountOnes02(num: 9))
+
+//判断一个数是否是2的整数次幂
+//如果一个数是2的整数次幂,那么这个数的二进制表示中只有一位是1
+//func isPowerOfTwo(num: UInt) -> Bool {
+//    return (num & (num - 1)) == 0
+//}
+//print(isPowerOfTwo(num: 7))
+
+
+//小时的数字,一个数组里面有两两出现的数字,其中一个数字消失,找出这个消失的数字
+//相同的数字异或结果为0
+//func findLostNum(nums:[UInt]) -> UInt {
+//    var lostNum: UInt = 0
+//    for num in nums {
+//        lostNum = lostNum ^ num
+//    }
+//    return lostNum
+//}
+//var nums: [UInt] = [1,2,3,4,5,5,3,1,2]
+//print(findLostNum(nums: nums))
+
+//找到两个消失的数字
+//将所有的数字分为两个组,一个组有一个消失的数字,两个数字必定有一位是不同的,按不同位进行分组
+//func findLostTwoNum(nums: [UInt]) -> (UInt, UInt) {
+//    var lostNum1: UInt = 0
+//    var lostNum2: UInt = 0
+//    //先得到所有数字异或的结果
+//    var temp: UInt = 0
+//    for num in nums {
+//        temp = temp ^ num
+//    }
+//    //得到两个数字不同的位数
+//    var flag: UInt = 1
+//    while (flag & temp) == 0 {
+//        flag = flag << 1
+//    }
+//    //对数据进行分组异或
+//    for num in nums {
+//        if (num & flag) == 1 {
+//            lostNum1 = lostNum1 ^ num
+//        }
+//        else {
+//            lostNum2 = lostNum2 ^ num
+//        }
+//    }
+//    return (lostNum1, lostNum2)
+//}
+//var nums01: [UInt] = [1,2,3,4,5,3,2,5]
+//
+//print(findLostTwoNum(nums: nums01))
+
+//数组中只有一个数出现一次,其他的都出现三次,找出这个数字
+
+//func findOnceNum(nums: [UInt]) -> UInt {
+//    var a: UInt = 0
+//    var b: UInt = 0
+//    for num in nums {
+//        a = (a ^ num) & ~b
+//        b = (b ^ num) & ~a
+//    }
+//    return a
+//}
+//var nums02: [UInt] = [1,1,1,2,2,2,6]
+//print(findOnceNum(nums: nums02))
+
+//运算符的重载
+struct Vecter2D: Equatable {
+    var x = 0.0
+    var y = 0.0
 }
-var b: Int? = 10
-print(b ?? getNumber()) //方法没有被执行
-b = nil
-print(b ?? getNumber()) //方法被执行
+
+extension Vecter2D {
+    static func + (left: Vecter2D, right: Vecter2D) -> Vecter2D {
+        return Vecter2D(x: left.x + right.x, y: left.y + right.y)
+    }
+}
+
+extension Vecter2D {
+    static prefix func - (v: Vecter2D) -> Vecter2D {
+        return Vecter2D(x: -v.x, y: -v.y)
+    }
+}
+//需要修改外部变量的值需要添加inout参数
+extension Vecter2D {
+    static func += (left: inout Vecter2D, right: Vecter2D) {
+        left =  left + right
+    }
+}
+//等价运算符的重载需要遵循Equatable协议
+//extension Vecter2D: Equatable {
+//    static func == (left: Vecter2D, right: Vecter2D) -> Bool {
+//        return left.x == right.x && left.y == right.y
+//    }
+//}
+
+//var v1 = Vecter2D(x: 2, y: 2)
+//let v2 = Vecter2D(x: 2, y: 2)
+//print(v1 + v2)
+//print(-v1)
+//v1 += v2
+//print(v1,v2)
+//print(v1 == v2)
+
+//自定义前置运算符
+prefix operator +++
+extension Vecter2D {
+    static prefix func +++(vecter: inout Vecter2D) -> Vecter2D {
+        vecter += vecter
+        return vecter
+    }
+}
+
+//var v1 = Vecter2D(x: 1, y: 1)
+//var v2 = +++v1
+//print(v1,v2)
+
+//自定义中置运算符
+infix operator +-: AdditionPrecedence
+extension Vecter2D {
+    static func +-(v1: Vecter2D, v2: Vecter2D) -> Vecter2D {
+        return Vecter2D(x: v1.x + v2.x, y: v1.y - v2.y)
+    }
+}
+
+infix operator *^: MyPrecedence
+extension Vecter2D {
+    static func *^(left: Vecter2D, right: Vecter2D) -> Vecter2D {
+        return Vecter2D(x: left.x * right.x, y: left.y * left.y + right.y * right.y)
+    }
+}
+//自定义优先级和结合性
+precedencegroup MyPrecedence {
+    associativity: left
+    lowerThan: AdditionPrecedence
+}
+
+var v1 = Vecter2D(x: 1, y: 1)
+var v2 = Vecter2D(x: 2, y: 2)
+var v3 = Vecter2D(x: 3, y: 3)
+print(v1 +- v2 *^ v3)
+
+
+
+
