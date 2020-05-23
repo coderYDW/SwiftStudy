3ca63deb9caf2b822ea610f33415fce3101c05da
DovYoung <1591759055@qq.com>
2020-05-23 11:46:25 +0800

    模式分类

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -563,94 +563,175 @@ import PlaygroundSupport
 //}
 
 //if语句使用不当,导致嵌套层数很深,阅读很不利
-func isIpAddress01(ipString: String) -> (Int, String) {
-    let components = ipString.split(separator: ".")
-    if components.count == 4 {
-        if let first = Int(components[0]), first >= 0 && first <= 255 {
-            if let second = Int(components[1]), second >= 0 && second <= 255 {
-                if let third = Int(components[2]), third >= 0 && third <= 255 {
-                    if let forth = Int(components[3]), forth >= 0 && forth <= 255 {
-                        return (0, "IP地址合法")
-                    }
-                    else {
-                       return (3, "第四部分不对")
-                    }
-                }
-                else {
-                   return (3, "第三部分不对")
-                }
-            }
-            else {
-               return (2, "第二部分不对")
-            }
-        }
-        else {
-            return (1, "第一部分不对")
-        }
-    }
-    else {
-        return (100, "不是IP地址,不是由四部分组成")
-    }
-}
+//func isIpAddress01(ipString: String) -> (Int, String) {
+//    let components = ipString.split(separator: ".")
+//    if components.count == 4 {
+//        if let first = Int(components[0]), first >= 0 && first <= 255 {
+//            if let second = Int(components[1]), second >= 0 && second <= 255 {
+//                if let third = Int(components[2]), third >= 0 && third <= 255 {
+//                    if let forth = Int(components[3]), forth >= 0 && forth <= 255 {
+//                        return (0, "IP地址合法")
+//                    }
+//                    else {
+//                       return (3, "第四部分不对")
+//                    }
+//                }
+//                else {
+//                   return (3, "第三部分不对")
+//                }
+//            }
+//            else {
+//               return (2, "第二部分不对")
+//            }
+//        }
+//        else {
+//            return (1, "第一部分不对")
+//        }
+//    }
+//    else {
+//        return (100, "不是IP地址,不是由四部分组成")
+//    }
+//}
+//
+//func isIpAddress02(ipString: String) -> (Int, String) {
+//
+//    let components = ipString.split(separator: ".")
+//
+//    if components.count != 4 {
+//        return (100, "不是IP地址,不是由四部分组成")
+//    }
+//
+//    if let first = Int(components[0]), first < 0 || first > 255 {
+//        return (1, "第一部分不对")
+//    }
+//
+//    if let second = Int(components[1]), second < 0 || second > 255 {
+//        return (2, "第二部分不对")
+//    }
+//
+//    if let third = Int(components[2]), third < 0 || third > 255 {
+//        return (3, "第三部分不对")
+//    }
+//
+//    if let forth = Int(components[3]), forth < 0 || forth > 255 {
+//        return (3, "第四部分不对")
+//    }
+//
+//    return (0, "IP地址合法")
+//
+//}
+//
+//
+////guard语句书写更加直观,避免嵌套
+//func isIpAdressUseGuard(ipString: String) -> (Int, String) {
+//
+//    let components = ipString.split(separator: ".")
+//
+//    guard components.count == 4 else {
+//        return (100, "不是IP地址,不是由四部分组成")
+//    }
+//
+//    guard let first = Int(components[0]), first >= 0 && first <= 255 else {
+//        return (1, "第一部分不对")
+//    }
+//
+//    guard let second = Int(components[1]), second >= 0 && second <= 255 else {
+//        return (1, "第二部分不对")
+//    }
+//
+//    guard let third = Int(components[2]), third >= 0 && third <= 255 else {
+//        return (2, "第三部分不对")
+//    }
+//
+//    guard let forth = Int(components[3]), forth >= 0 && forth <= 255 else {
+//        return (3, "第四部分不对")
+//    }
+//
+//    return (0, "IP地址合法")
+//
+//}
+//
+////print(isIpAdressUseGuard(ipString: "101.110.91.110"))
+//print(isIpAddress02(ipString: "10.91.91.101"))
 
-func isIpAddress02(ipString: String) -> (Int, String) {
-    
-    let components = ipString.split(separator: ".")
-    
-    if components.count != 4 {
-        return (100, "不是IP地址,不是由四部分组成")
-    }
-    
-    if let first = Int(components[0]), first < 0 || first > 255 {
-        return (1, "第一部分不对")
-    }
-    
-    if let second = Int(components[1]), second < 0 || second > 255 {
-        return (2, "第二部分不对")
-    }
-    
-    if let third = Int(components[2]), third < 0 || third > 255 {
-        return (3, "第三部分不对")
-    }
-    
-    if let forth = Int(components[3]), forth < 0 || forth > 255 {
-        return (3, "第四部分不对")
-    }
-    
-    return (0, "IP地址合法")
-    
-}
+//可选项模式
+//let someInt: Int? = 42
+//if case let x? = someInt {
+//    print(x)
+//}
+//else {
+//    print("no value")
+//}
+
+//只打印数组中不为nil元素
+//let nums: [Int?] = [nil, 1, 78, 90, nil, 67]
+//for case let x? in nums {
+//    print(x)
+//}
+
+//枚举项模式
+//let someInt: Int? = 42
+//if case .some(let x) = someInt {
+//    print(x)
+//}
+//else {
+//    print("no value")
+//}
+
+////类型转换模式
+//protocol Animal {
+//    var name: String {get} //只有get方法
+//}
+//
+//struct Dog: Animal {
+//    var name: String = "Dog"
+//    var runSpeed: Int
+//}
+//
+//struct Bird: Animal {
+//    var name: String = "Bird"
+//    var flyHeight: Int
+//}
+//
+//struct Fish: Animal {
+//    var name: String = "Fish"
+//    var Depth: Int
+//}
+//
+//let animals: [Animal] = [Dog(runSpeed: 100), Bird(flyHeight: 50), Fish(Depth: 150)]
+//for animal in animals {
+//    switch animal {
+//    case let dog as Dog:
+//        print("\(dog.name) run \(dog.runSpeed)")
+//    case let bird as Bird:
+//        print("\(bird.name) fly \(bird.flyHeight)")
+//    case is Fish:
+//        print("Fish can swim")
+//    default:
+//        print("unknow animal")
+//    }
+//}
 
 
-//guard语句书写更加直观,避免嵌套
-func isIpAdressUseGuard(ipString: String) -> (Int, String) {
-    
-    let components = ipString.split(separator: ".")
-    
-    guard components.count == 4 else {
-        return (100, "不是IP地址,不是由四部分组成")
-    }
-    
-    guard let first = Int(components[0]), first >= 0 && first <= 255 else {
-        return (1, "第一部分不对")
-    }
-    
-    guard let second = Int(components[1]), second >= 0 && second <= 255 else {
-        return (1, "第二部分不对")
-    }
-    
-    guard let third = Int(components[2]), third >= 0 && third <= 255 else {
-        return (2, "第三部分不对")
-    }
-    
-    guard let forth = Int(components[3]), forth >= 0 && forth <= 255 else {
-        return (3, "第四部分不对")
-    }
-    
-    return (0, "IP地址合法")
-    
-}
-
-//print(isIpAdressUseGuard(ipString: "101.110.91.110"))
-print(isIpAddress02(ipString: "10.91.91.101"))
+////表达式模式
+//struct Teacher {
+//    var name: String
+//    var salary: Int
+//}
+//
+//func ~= (patten: Range<Int>, value: Teacher) -> Bool {
+//    return patten.contains(value.salary)
+//}
+//
+//let teacher = Teacher(name: "zhang", salary: 9000)
+//switch teacher {
+//case 0..<1000:
+//    print("饿死")
+//case 1000..<3000:
+//    print("勉强度日")
+//case 3000..<6000:
+//    print("还可以")
+//default:
+//    print("土豪")
+//}
 
