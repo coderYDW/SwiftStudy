ba51ad5afad9c6951554fb8f8d04c1c555a974d5
杨冬武 <coderYDW@users.noreply.github.com>
2020-07-01 11:11:20 +0800

    GCD的简单操作
    DispatchQueue
    DispatchGroup
    DispatchSource.makeTimerSource

--- Thread.playground/Contents.swift
+++ Thread.playground/Contents.swift
@@ -59,17 +59,69 @@ class MyOperation: Operation {
 //print("after invoke test")
 
 
-//Dispatch
-
+//DispatchQueue
+//创建队列
 let queue = DispatchQueue(label: "MyQueue", qos: DispatchQoS.default, attributes: DispatchQueue.Attributes.concurrent, autoreleaseFrequency: DispatchQueue.AutoreleaseFrequency.inherit, target: nil)
 
-//queue.async {
+//同步执行
+//queue.sync {
 //    sleep(1)
 //    print("in queue sync")
 //}
 
-queue.asyncAfter(deadline: .now() + 1) {
-    print("asyncAfter")
+//异步执行
+//queue.async {
+//    sleep(1)
+//    print("in queue async")
+//}
+
+//异步的延时执行
+//queue.asyncAfter(deadline: .now() + 2) {
+//    print("asyncAfter")
+//}
+
+//print("after invoke queue method")
+
+//DispatchGroup
+
+let group = DispatchGroup()
+
+group.enter()
+queue.async {
+    sleep(1)
+    print("A 完成")
+    group.leave()
 }
 
-print("after invoke queue method")
+group.enter()
+queue.async {
+    sleep(1)
+    print("B 完成")
+    group.leave()
+}
+
+print("group enter 完成")
+
+//wait 阻塞流程
+group.wait()
+
+//notify 不阻塞流程
+//group.notify(queue: queue) {
+//    print("notify 完成")
+//}
+
+print("验证堵塞")
+
+//Timer
+var seconds = 10
+let timer = DispatchSource.makeTimerSource(flags:[], queue: queue)
+timer.schedule(deadline: .now(), repeating: 1.0)
+timer.setEventHandler {
+    seconds -= 1
+    if seconds < 0 {
+        timer.cancel()
+    } else {
+        print(seconds)
+    }
+}
+timer.resume()
